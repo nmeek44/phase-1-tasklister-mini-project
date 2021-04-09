a8128ec4c6ef582dcf1de672e15ba5228e3406d0
Yosef <shmuli5770@gmail.com>
2021-04-09 18:10:21 -0400

    I did things

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let form = document.querySelector("#create-task-form")
-  let theList = document.querySelector("#tasks")
-   console.log(form);
-   form.addEventListener("submit", (iceCream) => {
-      iceCream.preventDefault()
-      let result = iceCream.target["new-task-description"].value
-      let newTask = document.createElement("li")
-      newTask.innerText = result
-      theList.append(newTask)
-      console.log(theList)
-   } )
-});
+let prioritySelector = document.querySelector("#priority")
+let form = document.querySelector("#create-task-form")
+let theList = document.querySelector("#tasks")
+let priorityHigh = 'High', priorityMedium = 'Medium', priorityLow = 'Low'
+let mixedArray = []
+form.addEventListener("submit", (iceCream) => {
+   iceCream.preventDefault()
+   let result = iceCream.target["new-task-description"].value
+   let newTask = document.createElement("li")
+   let deleteBtn = document.createElement("button")
+   let chosenPriority = iceCream.target['priority-list'].value
+   deleteBtn.innerText = 'X'
+   newTask.innerText = `${result} `
+   if (chosenPriority === priorityHigh) {
+      newTask.style.color = 'red'
+   } else if (chosenPriority === priorityMedium) {
+      newTask.style.color = 'yellow'
+   } else {
+      newTask.style.color = 'green'
+   }
+   newTask.append(deleteBtn)
+   deleteBtn.addEventListener('click', () => {
+      newTask.remove()
+   })
+   mixedArray.push(newTask)
+   theList.append(newTask)
+})
+console.log(mixedArray)
