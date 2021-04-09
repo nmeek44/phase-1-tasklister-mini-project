cdf28a4111f231f17fc8473d1b3508b2af034b5c
nmeek44 <nmeek44@gmail.com>
2021-04-09 15:23:50 -0400

    part 1 finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form")
+  let theList = document.querySelector("#tasks")
+   console.log(form);
+   form.addEventListener("submit", (iceCream) => {
+      iceCream.preventDefault()
+      let result = iceCream.target["new-task-description"].value
+      let newTask = document.createElement("li")
+      newTask.innerText = result
+      theList.append(newTask)
+      console.log(theList)
+   } )
 });
